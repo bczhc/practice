193cceb6c1142118e11cda1a3bdd627ee4a37bd0
bczhc <bczhc0@126.com>
2020-08-28 13:56:35 +0800

    update BubbleSort

--- sort/BubbleSort.cpp
+++ sort/BubbleSort.cpp
@@ -4,8 +4,8 @@
 template <typename T>
 void bubbleSort(T arr[], int length, Comparable<T> &comparable) {
     int temp;
-    for (int j = 0; j < length; ++j) {
-        for (int i = 0; i < length - 1 - j; ++i) {
+    for (int j = length - 1; j > 0; --j) {
+        for (int i = 0; i < j; ++i) {
             if (comparable.compare(arr[i], arr[i + 1]) > 0) {
                 temp = arr[i + 1];
                 arr[i + 1] = arr[i];
@@ -23,7 +23,8 @@ int main() {
     } c;
     bubbleSort<int>(arr, 6, c);
     for (int i = 0; i < 6; ++i) {
-        printf("%i ", arr[i]), putchar('\n');
+        printf("%i ", arr[i]);
     }
+    putchar('\n');
     return 0;
 }
